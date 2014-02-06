24d00fcc9ea649e769a1ae4ff94afd79ed0d4d92
Kevin Obee <kevin.obee@gmail.com>
2014-02-06 17:54:56 +0000

    Integrate latest Speak.Services to use BaseTypeProvider<T>

--- src/Speak.Service.Contrib/MetaData/ValidationMetaDataTypeProvider.cs
+++ src/Speak.Service.Contrib/MetaData/ValidationMetaDataTypeProvider.cs
@@ -1,52 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using Speak.Service.Core;
+﻿using System.Reflection;
 using Speak.Service.Core.MetaData;
+using Speak.Service.Core.Reflection;
 
 namespace Speak.Service.Contrib.MetaData
 {
-    public class ValidationMetaDataTypeProvider : ITypeProvider
+    public class ValidationMetaDataTypeProvider : BaseTypeProvider<IValidationMetaData>
     {
-        private readonly Assembly[] _assemblies;
-
-        public ValidationMetaDataTypeProvider(Assembly[] assemblies)
+        public ValidationMetaDataTypeProvider(Assembly[] assemblies) : base(assemblies)
         {
-            _assemblies = assemblies;
-        }
-
-        public Type[] Types
-        {
-            get { return ValidationMetaDataTypes(); }
-        }
-
-        private Type[] ValidationMetaDataTypes()
-        {
-            var validators =
-                from assembly in _assemblies
-                where !assembly.IsDynamic
-                from type in GetTypesInAssembly(assembly)
-                where !type.IsAbstract
-                where !type.IsGenericTypeDefinition
-                where typeof(IValidationMetaData).IsAssignableFrom(type)
-                select type;
-
-            return validators.ToArray();
-        }
-
-        private static IEnumerable<Type> GetTypesInAssembly(Assembly assembly)
-        {
-            var noTypesFound = new Type[] { };
-
-            try
-            {
-                return assembly.GetTypes();
-            }
-            catch (ReflectionTypeLoadException)
-            {
-                return noTypesFound;
-            }
         }
     }
 }
