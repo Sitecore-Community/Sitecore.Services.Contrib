ba101710b67fdcb7861ddfc6e0b97bc851a67bd3
Kevin Obee <kevin.obee@gmail.com>
2014-03-27 09:54:14 +0000

    Rename SPEAKentityservice to EntityService in emailAddress.js

--- src/Sitecore.Services.Contrib/content/sitecore/shell/client/Services/Assets/lib/extensions/validators/emailAddress.js
+++ src/Sitecore.Services.Contrib/content/sitecore/shell/client/Services/Assets/lib/extensions/validators/emailAddress.js
@@ -1,3 +1,3 @@
-﻿SPEAKentityservice.utils.validator.add( "emailAddress", function ( value, params ) {
+﻿EntityService.utils.validator.add("emailAddress", function (value, params) {
   return /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test( value );
 } );
