2dc62c8537ad1df555381449466bc884976cf16f
Kevin Obee <kevin.obee@gmail.com>
2014-04-22 15:13:27 +0100

    NoItemServiceRouteMapper  added

---
+++ src/Sitecore.Services.Contrib/Web/Http/NoItemServiceRouteMapper.cs
@@ -0,0 +1,47 @@
+﻿using System.Web.Http;
+using System.Web.Routing;
+
+using Sitecore.Services.Infrastructure.Sitecore.Configuration;
+using Sitecore.Services.Infrastructure.Web.Http;
+
+namespace Sitecore.Services.Contrib.Web.Http
+{
+  public class NoItemServiceRouteMapper : IMapRoutes
+  {
+    private readonly IMapRoutes _routeMapper;
+
+    public NoItemServiceRouteMapper(IMapRoutes routeMapper)
+    {
+      _routeMapper = routeMapper;
+    }
+
+    /// <summary>
+    /// Default constructor required to for CreateInstance in ConfigurationRouteConfigurationFactory to work
+    /// </summary>
+    public NoItemServiceRouteMapper()
+      : this(new DefaultRouteMapper(new ServicesSettingsConfigurationProvider().Configuration.Services.Routes.RouteBase))
+    {
+    }
+
+    public void MapRoutes(HttpConfiguration config)
+    {
+      _routeMapper.MapRoutes(config);
+
+      config.Routes.Remove(DefaultRouteMapper.RouteName.ItemService.Children);
+      config.Routes.Remove(DefaultRouteMapper.RouteName.ItemService.ContentPath);
+      config.Routes.Remove(DefaultRouteMapper.RouteName.ItemService.Default);
+      config.Routes.Remove(DefaultRouteMapper.RouteName.ItemService.Path);
+      config.Routes.Remove(DefaultRouteMapper.RouteName.ItemService.Query);
+      config.Routes.Remove(DefaultRouteMapper.RouteName.ItemService.QueryViaItem);
+      config.Routes.Remove(DefaultRouteMapper.RouteName.ItemService.Search);
+      config.Routes.Remove(DefaultRouteMapper.RouteName.ItemService.SearchViaItem);
+    }
+
+    public void MapRoutes(RouteCollection routes)
+    {
+      _routeMapper.MapRoutes(routes);
+
+      routes.Remove(routes[DefaultRouteMapper.RouteName.Authentication]);
+    }
+  }
+}
---
+++ test/unit-test/Sitecore.Services.Contrib.Test/Web/Http/NoItemServiceTests.cs
@@ -0,0 +1,112 @@
+﻿using System.Web.Http;
+using System.Web.Routing;
+using Sitecore.Services.Contrib.Web.Http;
+using Sitecore.Services.Infrastructure.Web.Http;
+
+using Should;
+using Xunit;
+
+namespace Sitecore.Services.Contrib.Test.Web.Http
+{
+  public class NoItemServiceTest
+  {
+    protected readonly NoItemServiceRouteMapper Sut;
+
+    public NoItemServiceTest()
+    {
+      var defaultMapper = new DefaultRouteMapper("");
+
+      Sut = new NoItemServiceRouteMapper(defaultMapper);      
+    }
+  }
+
+  public class NoItemServiceRouteMapperBehaviour
+  {
+    [Fact]
+    public void mapper_implements_IMapRoutes()
+    {
+      typeof(IMapRoutes).IsAssignableFrom(typeof(NoItemServiceRouteMapper)).ShouldBeTrue();
+    }
+  }
+
+  public class NoItemServiceRouteMapperHttpConfigurationBehaviour : NoItemServiceTest
+  {
+    private readonly HttpConfiguration _httpConfiguration;
+    private readonly RouteCollection _routeCollection;
+
+    public NoItemServiceRouteMapperHttpConfigurationBehaviour()
+    {
+      _httpConfiguration = new HttpConfiguration();
+      _routeCollection = new RouteCollection();
+
+      Sut.MapRoutes(_httpConfiguration);
+      Sut.MapRoutes(_routeCollection);
+    }
+
+    [Fact]
+    public void entityservice_route_is_mapped()
+    {
+      _httpConfiguration.Routes.ContainsKey(DefaultRouteMapper.RouteName.EntityService.IdAction).ShouldBeTrue();
+    }
+
+    [Fact]
+    public void entityservice_metadata_script_route_is_mapped()
+    {
+      _routeCollection[DefaultRouteMapper.RouteName.EntityService.MetaDataScript].ShouldNotBeNull();
+    }
+
+    [Fact]
+    public void itemservice_query_route_not_mapped()
+    {
+      _httpConfiguration.Routes.ContainsKey(DefaultRouteMapper.RouteName.ItemService.Query).ShouldBeFalse();
+    }
+
+    [Fact]
+    public void itemservice_query_via_item_route_not_mapped()
+    {
+      _httpConfiguration.Routes.ContainsKey(DefaultRouteMapper.RouteName.ItemService.QueryViaItem).ShouldBeFalse();
+    }
+
+    [Fact]
+    public void itemservice_search_route_not_mapped()
+    {
+      _httpConfiguration.Routes.ContainsKey(DefaultRouteMapper.RouteName.ItemService.Search).ShouldBeFalse();
+    }
+
+    [Fact]
+    public void itemservice_search_via_item_route_not_mapped()
+    {
+      _httpConfiguration.Routes.ContainsKey(DefaultRouteMapper.RouteName.ItemService.SearchViaItem).ShouldBeFalse();
+    }
+
+    [Fact]
+    public void itemservice_children_route_not_mapped()
+    {
+      _httpConfiguration.Routes.ContainsKey(DefaultRouteMapper.RouteName.ItemService.Children).ShouldBeFalse();
+    }
+
+    [Fact]
+    public void itemservice_default_route_not_mapped()
+    {
+      _httpConfiguration.Routes.ContainsKey(DefaultRouteMapper.RouteName.ItemService.Default).ShouldBeFalse();
+    }
+
+    [Fact]
+    public void itemservice_content_path_route_not_mapped()
+    {
+      _httpConfiguration.Routes.ContainsKey(DefaultRouteMapper.RouteName.ItemService.ContentPath).ShouldBeFalse();
+    }
+
+    [Fact]
+    public void itemservice_path_route_not_mapped()
+    {
+      _httpConfiguration.Routes.ContainsKey(DefaultRouteMapper.RouteName.ItemService.Path).ShouldBeFalse();
+    }
+
+    [Fact]
+    public void authentication_route_not_mapped()
+    {
+      _routeCollection[DefaultRouteMapper.RouteName.Authentication].ShouldBeNull();
+    }
+  }
+}
